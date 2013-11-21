d70d41c5e02deed548e9c38533d9bc65b457751f
Chalkias Tasos <imtasos@gmail.com>
2013-11-21 12:48:50 +0200

    Add UUID generation capabilities.
    Signed-off-by: Chalkias Tasos <imtasos@gmail.com>

---
+++ lib/PasswordLib/Uuid/UuidGenerationException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PasswordLib\Uuid;
+
+/**
+ * Indicates an error that occurred during the generation of a UUID.
+ *
+ * @package PasswordLib\Uuid
+ * @author ChASM <info@chasm.gr>
+ */
+class UuidGenerationException extends \Exception {
+
+} 
